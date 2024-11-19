3bd8056a0ec72456a076d0bc003e395ceb7e6f0b
cindy <cindytat@dpengineering.org>
2024-11-19 10:30:47 -0800

    I added DPi stepper code

--- main.py
+++ main.py
@@ -19,6 +19,36 @@ from pidev.kivy import DPEAButton
 from pidev.kivy import ImageButton
 from pidev.kivy.selfupdatinglabel import SelfUpdatingLabel
 
+from dpeaDPi.DPiComputer import DPiComputer
+from dpeaDPi.DPiStepper import *
+from time import sleep
+
+dpiStepper = DPiStepper()
+dpiStepper.setBoardNumber(0)
+
+if dpiStepper.initialize() != True:
+    print("Communication with the DPiStepper board failed.")
+
+dpiStepper.enableMotors(True)
+
+microstepping = 8
+dpiStepper.setMicrostepping(microstepping)
+
+speed_steps_per_second = 200 * microstepping
+accel_steps_per_second_per_second = speed_steps_per_second
+dpiStepper.setSpeedInStepsPerSecond(0, speed_steps_per_second)
+dpiStepper.setSpeedInStepsPerSecond(1, speed_steps_per_second)
+dpiStepper.setAccelerationInStepsPerSecondPerSecond(0, accel_steps_per_second_per_second)
+dpiStepper.setAccelerationInStepsPerSecondPerSecond(1, accel_steps_per_second_per_second)
+
+stepperStatus = dpiStepper.getStepperStatus(0)
+print(f"Pos = {stepperStatus}")
+
+stepper_num = 0
+steps = 1600
+wait_to_finish_moving_flg = True
+dpiStepper.moveToRelativePositionInSteps(stepper_num, steps, wait_to_finish_moving_flg)
+
 from datetime import datetime
 
 time = datetime
